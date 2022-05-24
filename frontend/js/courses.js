8964c95bb848fc9e14c7ede94cd136cb112e8f2d
gabrielyin <70323043+gabrielyin@users.noreply.github.com>
2022-05-24 09:17:42 -0300

    trazendo info do back pro front com fetch

---
+++ frontend/js/courses.js
@@ -0,0 +1,11 @@
+const feedDisplay = document.getElementById('feed');
+
+fetch('http://localhost:3000/logindo')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(user => {
+            const nome = `<h3>` + user.nome + `</h3>`
+            feedDisplay.insertAdjacentHTML("beforeend", nome)
+        })
+    })
+    .catch(err => console.log(err))
